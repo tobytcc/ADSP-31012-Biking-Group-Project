b0131fbb587c8eda8745f0df7e38b8137826b75d
tobytcc <tobytcc@gmail.com>
2023-11-15 22:36:48 -0600

    DML

--- database/databaseDML.sql
+++ database/databaseDML.sql
@@ -5,25 +5,15 @@ USE bikes;
 
 --- Uploading Data from csv file ---
 # Smart Location Data
-LOAD DATA LOCAL INFILE "C:/Users/..."
+LOAD DATA LOCAL INFILE "C:/Users/tobyt/Documents/UChicago Coding/ADSP 31012 Data Engineering Platforms for Analytics/ADSP-31012-Biking-Group-Project/data/EPA_SmartLocationDatabase_V3_Jan_2021_Final.csv"
 INTO TABLE smartlocation_original
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 
-# Location Lookup 
-LOAD DATA LOCAL INFILE "C:/Users/..."
-INTO TABLE locations_original
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS  ;
-ALTER TABLE locations
-DROP COLUMN geoid;
-
 # Bike Racks
-LOAD DATA LOCAL INFILE "C:/Users/..."
+LOAD DATA LOCAL INFILE "C:/Users/tobyt/Documents/UChicago Coding/ADSP 31012 Data Engineering Platforms for Analytics/ADSP-31012-Biking-Group-Project/data/Bike_Racks.csv"
 INTO TABLE bikeracks_original
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -31,7 +21,7 @@ LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 
 # Crash Data
-LOAD DATA LOCAL INFILE "C:/Users/..."
+LOAD DATA LOCAL INFILE "C:/Users/tobyt/Documents/UChicago Coding/ADSP 31012 Data Engineering Platforms for Analytics/ADSP-31012-Biking-Group-Project/data/Traffic_Crashes_-_Crashes (1).csv"
 INTO TABLE crashdata_original
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -39,7 +29,7 @@ LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 
 # Divvy Stations
-LOAD DATA LOCAL INFILE "C:/Users/..."
+LOAD DATA LOCAL INFILE "C:/Users/tobyt/Documents/UChicago Coding/ADSP 31012 Data Engineering Platforms for Analytics/ADSP-31012-Biking-Group-Project/data/Divvy_Bicycle_Stations.csv"
 INTO TABLE divvystations_original
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -47,12 +37,14 @@ LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 
 # Divvy Trips
-LOAD DATA LOCAL INFILE "C:/Users/..."
+LOAD DATA LOCAL INFILE "C:/Users/tobyt/Documents/UChicago Coding/ADSP 31012 Data Engineering Platforms for Analytics/ADSP-31012-Biking-Group-Project/data/202309-divvy-tripdata.csv"
 INTO TABLE divvytrips_original
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
+
+SELECT * FROM divvytrips_original;
 
 --- Coordinate Rounding ---
 SET sql_safe_updates = 0;
@@ -76,10 +68,12 @@ UPDATE divvytrips_original
 SET start_lat = ROUND(start_lat, 4),
     start_lng = ROUND(start_lng, 4),
     end_lat = ROUND(end_lat, 4),
-    end_lng = ROUND(end_lng, 4),
+    end_lng = ROUND(end_lng, 4)
 WHERE ride_id IS NOT NULL;
 
 --- Inserting foreign key data ---
+
+# Location Lookup 
 UPDATE locations_original l
 JOIN smartlocation_original sl ON 
   l.state = sl.statefp
@@ -87,6 +81,10 @@ JOIN smartlocation_original sl ON
   AND l.tract = sl.tractce
   AND l.blockgrp = sl.blkgrpce
 SET l.blk_grp_id = sl.blk_grp_id;
+ALTER TABLE locations
+DROP COLUMN geoid;
+
+SELECT * FROM locations_original;
 
 UPDATE crashdata_original c
 JOIN locations_original l ON 
@@ -113,8 +111,10 @@ JOIN locations_original l ON
 SET ds.start_loc_id = l.loc_id;
 
 UPDATE divvytrips_original dt
-JOIN locations_original l ON 
-  dt.end_lng = l.lng
+JOIN locations_original l ON
+  dt.start_lat = l.lat
+  AND dt.start_lng = l.lng
+  AND dt.end_lng = l.lng
   AND dt.end_lat = l.lat
 SET ds.end_loc_id = l.loc_id;
 
